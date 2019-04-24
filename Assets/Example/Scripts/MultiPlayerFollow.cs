09efd0ea3ec7295541dc04d24db4c7cd83f07b56
Ben C <ben@expmaker.com>
2019-04-23 20:21:50 -0500

    initial commit

---
+++ Assets/Example/Scripts/MultiPlayerFollow.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MultiPlayerFollow : MonoBehaviour
+{
+    PlayerController[] players;
+    Vector3 _cameraOffset = new Vector3(0, 1.414f, 1.414f);
+    float minDist = 4f;
+    const float SmoothFactor = 10f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        players = FindObjectsOfType<PlayerController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void LateUpdate()
+    {
+        Vector3 average = Vector3.zero;
+
+        float maxDist = 0f;
+        foreach (var item in players)
+        {
+            foreach (var p in players)
+            {
+                float d = (p.transform.position - item.transform.position).magnitude;
+                if (d > maxDist) maxDist = d;
+            }
+            average += item.transform.position;
+        }
+        average *= (1f / (float)players.Length);
+
+        Vector3 newPos = average + _cameraOffset * Mathf.Pow(Mathf.Max(minDist, maxDist), 0.75f);
+
+        transform.position = Vector3.Slerp(transform.position, newPos, SmoothFactor);
+
+        transform.LookAt(average);
+    }
+}
---
+++ Assets/IsoMultiCam.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class IsoMultiCam : MonoBehaviour
+{
+    public Transform target;
+    public Vector3 offset;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = target.position + offset;
+        transform.forward = target.position - transform.position;
+    }
+}
