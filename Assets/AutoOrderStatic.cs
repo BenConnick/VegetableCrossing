d75d1b34a629e5a1bd756a068d97064275778cd3
Ben C <ben@expmaker.com>
2019-04-27 23:27:13 -0500

    keyboard shortcut for auto order, undo abililty

--- Assets/AutoOrderStatic.cs
+++ Assets/AutoOrderStatic.cs
@@ -1,10 +1,11 @@
 ﻿using UnityEngine;
+using UnityEditor;
 
 public class AutoOrderStatic : MonoBehaviour
 {
     public float yOffset;
     // Start is called before the first frame update
-    void Start()
+    public void Start()
     {
         GetComponent<SpriteRenderer>().sortingOrder = transform.GetSortOrder(yOffset);
     }
@@ -16,5 +17,19 @@ public static class OrderUtils
     public static int GetSortOrder(this Transform t, float yOffset = 0)
     {
         return Mathf.RoundToInt((t.position.y + yOffset) * 1000f) * -1;
+    }
+
+    [MenuItem("Tools/AutoOrder Immediate &o")]
+    public static void OrderAll()
+    {
+        AutoOrderStatic[] all = Object.FindObjectsOfType<AutoOrderStatic>();
+        foreach (var item in all)
+        {
+            var spriteRenderer = item.GetComponent<SpriteRenderer>();
+            Undo.RecordObject(spriteRenderer, "Order by y");
+            item.Start();
+            PrefabUtility.RecordPrefabInstancePropertyModifications(spriteRenderer);
+        }
+        Undo.FlushUndoRecordObjects();
     }
 }
